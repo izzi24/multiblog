ba1f36097366cbec0446c87dcc1e3434ece3ce08
izzi24 <adedapotope57@gmail.com>
2025-02-16 16:53:20 +0100

    macho

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -59,6 +59,46 @@ export default function Login() {
           </div>
         </div>
       </div>
+      <div className=" lg:hidden w-full gap-10 grid grid-cols-1 capitalizen px-20  pb-10 justify-between mt-20  text-start">
+        <div className=" w-[100%]  text-gray-800 text-2xl">
+          <p className="font-bold py-3">LOGIN</p>
+          <div className=" px-10 py-10 border  w-[100%]">
+            <p className="py-3 font-bold capitalize">email</p>
+            <input
+              className="w-full py-5 border px-5 "
+              type="Email"
+              placeholder="Email"
+            />
+            <p className="py-3 capitalize font-bold">password</p>
+            <input
+              className="w-full border py-5 px-5 "
+              type="password"
+              placeholder="password"
+            />
+            <div className="py-3">
+              <button className="py-4 px-8 border-2 border-white bg-[#ff4000] text-xl text-white font-bold">
+                LOGIN
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className=" text-gray-800 w-[100%] text-2xl">
+          <p className="text-2xl font-bold py-3">NEW CUSTOMER</p>
+          <div className=" px-10 py-10 border  w-[100%]  ">
+            <p className="py-3 font-bold capitalize">CREATE AN ACCOUNT</p>
+            <p className="text-lg tracking-wider">
+              Sign up for a free account at our store. Registration is quick and
+              easy. It allows you to be able to order from our shop. To start
+              shopping click register.
+            </p>
+            <div className="py-3">
+              <button className="py-4 px-8 border-2 border-white bg-[#ff4000] text-xl text-white font-bold">
+                CREATE ACCOUNT
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
